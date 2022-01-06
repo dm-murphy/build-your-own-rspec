10cd9e02a3f482f83dec2fd8bf9bc3906d6409d0
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-01-06 14:08:36 -0500

    Wrap it block inside describe

--- emphasize_spec.rb
+++ emphasize_spec.rb
@@ -4,6 +4,8 @@ def emphasize(text)
   "#{text.upcase}!"
 end
 
-it 'makes the text uppercase and adds an exclamation point' do
-  expect(emphasize('hello')).to eq('HELLO!')
+MySpec.describe 'emphasizing text' do
+  it 'makes the text uppercase and adds an exclamation point' do
+    expect(emphasize('hello')).to eq('HELLO!')
+  end
 end
